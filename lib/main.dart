320eaea291c8f02ca2be3a83136e55663368dbc6
Andrew Bell <fiixed@gmail.com>
2019-10-25 09:49:20 -0500

    added user product screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './screens/cart_screen.dart';
+import './screens/user_products_screen.dart';
 import './screens/orders_screen.dart';
 import './providers/products_provider.dart';
 import './providers/cart.dart';
@@ -36,6 +37,7 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
 
         },
       ),
---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_app_prototype/providers/products_provider.dart';
+import 'package:shopping_app_prototype/widgets/app_drawer.dart';
+import '../providers/products_provider.dart';
+import '../widgets/user_product_item.dart';
+
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+  const UserProductsScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: productsData.items.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Column(
+              children: <Widget>[
+                UserProductItem(productsData.items[index].title,
+                    productsData.items[index].imageUrl),
+                    Divider(),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
