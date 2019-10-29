0141992676b28a9929e3414fc33e168cf13588fe
Andrew Bell <fiixed@gmail.com>
2019-10-29 09:58:00 -0500

    added edit product screen

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatefulWidget {
+  static const routeName = '/edit-product';
+  
   const EditProductScreen({Key key}) : super(key: key);
 
   @override
@@ -14,7 +16,19 @@ class _EditProductScreenState extends State<EditProductScreen> {
       appBar: AppBar(
         title: Text('Edit Product'),
       ),
-      body: null,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -4,6 +4,7 @@ import 'package:shopping_app_prototype/providers/products_provider.dart';
 import 'package:shopping_app_prototype/widgets/app_drawer.dart';
 import '../providers/products_provider.dart';
 import '../widgets/user_product_item.dart';
+import '../screens/edit_product_screen.dart';
 
 
 class UserProductsScreen extends StatelessWidget {
@@ -19,7 +20,9 @@ class UserProductsScreen extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
           ),
         ],
       ),
