8f2ea573e2f473dca2febb67a55ebc2569ded781
Andrew Bell <fiixed@gmail.com>
2019-10-31 10:46:36 -0500

    delete product

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../screens/edit_product_screen.dart';
+import '../providers/products_provider.dart';
 
 class UserProductItem extends StatelessWidget {
   final String id;
   final String title;
   final String imageUrl;
+
 
   const UserProductItem(this.id, this.title, this.imageUrl);
 
@@ -28,7 +31,9 @@ class UserProductItem extends StatelessWidget {
                 color: Theme.of(context).primaryColor),
             IconButton(
                 icon: Icon(Icons.delete),
-                onPressed: () {},
+                onPressed: () {
+                  Provider.of<Products>(context, listen: false).deleteProduct(id);
+                },
                 color: Theme.of(context).errorColor),
           ],
         ),
