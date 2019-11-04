d4dec82979613dfdfb2f303f22e7ddc1ab7e51fd
Andrew Bell <fiixed@gmail.com>
2019-11-04 11:43:51 -0600

    fetch and set orders

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,9 +4,29 @@ import 'package:shopping_app_prototype/widgets/app_drawer.dart';
 import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders';
   const OrdersScreen({Key key}) : super(key: key);
+
+  @override
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  var _isLoading = false;
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero).then((_) async {
+      setState(() {
+        _isLoading = true;
+      });
+      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,14 +36,16 @@ class OrdersScreen extends StatelessWidget {
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (BuildContext context, int index) {
-          return OrderItem(
-            order: orderData.orders[index],
-          );
-        },
-      ),
+      body: _isLoading
+          ? CircularProgressIndicator()
+          : ListView.builder(
+              itemCount: orderData.orders.length,
+              itemBuilder: (BuildContext context, int index) {
+                return OrderItem(
+                  order: orderData.orders[index],
+                );
+              },
+            ),
     );
   }
 }
