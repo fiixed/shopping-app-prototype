99d04d25df5db1f5e433a6d85f82e08f34ea1fc5
Andrew Bell <fiixed@gmail.com>
2019-11-05 06:56:18 -0600

    http firebase error handling

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,26 +1,35 @@
 import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> _authenticate(String email, String password, String urlSegment) async {
-   final url =
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDR2igCaLxhwrW15LaUXzi2IiXvkqUIvgo';
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (e) {
+      throw e;
+    }
   }
 
   Future<void> signup(String email, String password) async {
@@ -53,6 +62,5 @@ class Auth with ChangeNotifier {
     //     },
     //   ),
     // );
-    
   }
 }
